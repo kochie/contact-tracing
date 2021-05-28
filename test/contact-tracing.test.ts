269ac5c48aae980b212a8df7ed13f200ea2b7ddb
Robert Koch <robert@kochie.io>
2021-05-28 17:43:52 +1000

    Initial commit

---
+++ test/contact-tracing.test.ts
@@ -0,0 +1,11 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ContactTracing from '../lib/contact-tracing-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new ContactTracing.ContactTracingStack(app, 'MyTestStack');
+    // THEN
+    const actual = app.synth().getStackArtifact(stack.artifactId).template;
+    expect(actual.Resources ?? {}).toEqual({});
+});
