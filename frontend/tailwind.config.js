a47a49531e33261703a8002890242d354a9a5960
Robert Koch <robert@kochie.io>
2021-06-28 02:29:21 +1000

    fixes tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
